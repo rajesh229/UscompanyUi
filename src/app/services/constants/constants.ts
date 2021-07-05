726485f9acc1ed7757791cf4065238ba72a09ad0
rajesh229 <pesarurajesh8@gmail.com>
2021-07-05 13:21:54 +0530

    constants

--- src/app/services/constants/constants.ts
+++ src/app/services/constants/constants.ts
@@ -8,3 +8,4 @@ export const empsave=apiUrl+"/Employess/empsave";
 export const getallusers=apiUrl+"/Employess/getallusers";
 export const removeuser=apiUrl+"/Employess/removeuser";
 export const getstaff=apiUrl+"/Employess/getstaffony";
+export const mailutl="";
